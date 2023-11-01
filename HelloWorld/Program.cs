c74075f84f3289f9d4b59f5394c4b20d60da454f
tunay <tunay@DESKTOP-01AR0DS>
2023-11-02 00:50:05 +0300

    Proje dosyası ekle.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World ");
+
+            Console.Write("Bu Satır");
+            Console.Write(" Birleşik Yazar");
+
+            //Console.WriteLine("Yorum Satırı");
+
+            /*Console.WriteLine("Toplu Yorum Satırı");
+            Console.WriteLine("Toplu Yorum Satırı");
+            Console.WriteLine("Toplu Yorum Satırı");*/
+            Console.ReadLine();
+        }
+    }
+}
